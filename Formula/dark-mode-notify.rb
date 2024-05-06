de8097e745bba85fd6a24f586d8a0327da33c582
Loong <wangl.cc@outlook.com>
2024-05-07 00:43:27 +0800

    dark-mode-notify: use my own fork
    Closes #1.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/dark-mode-notify.rb
+++ Formula/dark-mode-notify.rb
@@ -1,27 +1,20 @@
 class DarkModeNotify < Formula
   desc "Swift program running a command whenever the dark mode status changes"
-  homepage "https://github.com/bouk/dark-mode-notify"
-  url "https://github.com/bouk/dark-mode-notify/archive/4d7fe211f81c5b67402fad4bed44995344a260d1.tar.gz"
-  version "0.0.1"
-  sha256 "794d588c2f743b982214900fd565e0986badf42f829fd995962280dd585d33ec"
+  homepage "https://github.com/wangl-cc/dark-mode-notify"
+  url "https://github.com/wangl-cc/dark-mode-notify/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "91646efab5916f61814f929783926624ab4a5b9166de04556bc0f1c61947f4ea"
 
   depends_on :macos
 
   uses_from_macos "swift" => :build
 
   def install
-    swiftc_flags = %w[
-      -O
-      -whole-module-optimization
-      -lto=llvm-full
-    ]
-
-    system "swiftc", "main.swift", *swiftc_flags, "-o", "dark-mode-notify"
-    bin.install "dark-mode-notify"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/dark-mode-notify"
   end
 
   service do
-    run [opt_bin/"dark-mode-notify", "#{Dir.home}/.config/dark-mode-notify/notify"]
+    run [opt_bin/"dark-mode-notify", "--daemon"]
     keep_alive true
     working_dir Dir.home
     environment_variables PATH: std_service_path_env
